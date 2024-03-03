3879067554b4ca8278308fdd7c98d41d9c093b2c
carlossr404 <carlossr404@gmail.com>
2024-03-02 19:28:51 -0600

    light work. Mainly read the 'Quick Start' section of React website and added comments as my form of taking notes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /> 
+        {/* curly braces used to "escape" from JSX to JS.
+          * "logo" in line 8 is a variable that contains the source for the image that shall be displayed
+          * The curly braces around this comment allow this comment to be identified as a comment as opposed to an error*/}
+          
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
