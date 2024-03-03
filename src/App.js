205256513f57e5390c55a9ab2ad2ce5d49d6bc6c
carlossr404 <carlossr404@gmail.com>
2024-03-02 19:29:56 -0600

    ditto last commit, I just forgot to save some stuff

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" /> 
         {/* curly braces used to "escape" from JSX to JS.
           * "logo" in line 8 is a variable that contains the source for the image that shall be displayed
-          * The curly braces around this comment allow this comment to be identified as a comment as opposed to an error*/}
-          
+          * The curly braces around this comment allow this comment to be identified as a comment as opposed to an error
+          * Remember: CSS vars shall be surrounded w/ "", JS vars shall be surrounded w/ {} */}
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
