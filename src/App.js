c0c21a0c80c108909523ec1ce8d0cfb90fd4508f
Carlossr404 <47153697+Carlossr404@users.noreply.github.com>
2024-03-04 23:03:11 -0600

    set up the tic-tac-toe tutorial from React website. Will branch to track progress as best practice from now on

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+{/*Function that contains our website*/}
 function App() {
+
+  {/*Everything inside return is rendered as the website*/}
   return (
+    /* Everything in the website must be wrapped within an enclosing tag... In this case, the div tag.
+      * This is b/c only a single element may be returned  */
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        {/* curly braces used to "escape" from JSX to JS.
-          * "logo" in line 8 is a variable that contains the source for the image that shall be displayed
-          * The curly braces around this comment allow this comment to be identified as a comment as opposed to an error
-          * Remember: CSS vars shall be surrounded w/ "", JS vars shall be surrounded w/ {} */}
+      
+      <button className='square'>x</button>
+      <button className='square'>x</button>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
