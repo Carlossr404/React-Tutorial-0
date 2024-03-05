47bf366bbaf73bd80a55fc45ae8b0d98ec435a31
Carlossr404 <47153697+Carlossr404@users.noreply.github.com>
2024-03-04 23:06:11 -0600

    my bad. made some minor changes to the styles... I couldn't help myself

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     /* Everything in the website must be wrapped within an enclosing tag... In this case, the div tag.
       * This is b/c only a single element may be returned  */
     <div className="App">
-      
+
       <button className='square'>x</button>
       <button className='square'>x</button>
 
