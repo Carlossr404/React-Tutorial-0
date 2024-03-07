d0d0d6112695676084a91c2a2e14bf7c032336e9
Carlossr404 <47153697+Carlossr404@users.noreply.github.com>
2024-03-06 23:27:50 -0600

    added Game() as the export default function. cleaned up code and cleared warnings

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import './index.css';
 import { useState } from 'react';
 
-{/**calcWinner: Helper function that checks for a winner
+/**calcWinner: Helper function that checks for a winner
   * Input: squares - array of values in the squares
-  * Returns: "X", "O", or null */}
+  * Returns: "X", "O", or null */
 function calcWinner(squares){
+
   //array of lines- trupples of square indicies that will indicate a winner if they all match
   const lines = [
     [0, 1, 2],
@@ -29,10 +30,10 @@ function calcWinner(squares){
 
 }
 
-{/**Square Component: handles all square-related matters 
-   * Input: val- item/string/value to be displayed by square; onSquareClick- function to be run updating "val" once Square is clicked
-   * Return: Button, rendered as a square that displays  "val"
-  *Remember to use the " () => " syntax before the function call */}
+/**Square Component: handles all square-related matters 
+  * Input: val- item/string/value to be displayed by square; onSquareClick- function to be run updating "val" once Square is clicked
+  * Return: Button, rendered as a square that displays  "val"
+  *Remember to use the " () => " syntax before the function call */
 function Square({val, onSquareClick}) {
   return(
     <button 
@@ -44,14 +45,14 @@ function Square({val, onSquareClick}) {
   );
 }
 
-{/**App: Function that contains our website*/}
-function App() {
-  {/** setting up turns and array that will hold our board data */}
-  const [xNext, setXNext] = useState(true);
-  const [squares, setSquares]= useState(Array(9).fill(null));
+/**Board Component: renders board and handles all matters related to it
+  * Input: xNext- bool representing player turn, squares- state of the board as array of squares, onPlay- function that shall handle gameplay. Must take in array
+  * Return: Rendering of the board and 'status'- next player or winner, whichever is applicable
+  */
+function Board({xNext, squares, onPlay}) {
 
-  {/**handleCLick: updates squares array holding board's state 
-    * Input: i - index of square to be marked*/}
+  /**handleCLick: updates squares array holding board's state and calls onPlay() w/ nextSquares, the new state of the board
+    * Input: i - index of square to be marked*/
   function handleClick(i) {
     if(squares[i] || calcWinner(squares)){return;}    //make sure the square is empty and there's no winner yet. Return/Do nothing if it isn't
     const nextSquares = squares.slice();  //copy "squares" array
@@ -63,21 +64,22 @@ function App() {
       nextSquares[i]= "O";
     }
 
-    //update state
-    setSquares(nextSquares);
-    setXNext(!xNext);
+    onPlay(nextSquares);
   }
 
-  {/*Everything inside return is rendered as the website*/}
+  //Calculate winner. If there is one, display who it is. Else, display the next player
+  const winner = calcWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner: " + winner + " ! Refresh the page to play again.";
+  }else{
+    status = "Next player: " + (xNext ? "X" : "O");
+  }
+
   return (
-    /* Everything in the website must be wrapped within an enclosing tag... In this case, the div tag.
-      * This is b/c only a single element may be returned  */
-    <div className="App">
-      <header>Welcome to my version of React's Tic-Tac-Toe Tutorial. We're currently under construction 😅</header>
-      <h1 className='h1'>Apologies for the delay. Here's a cat meme to compensate...</h1>
-
-      <img src= "./cat-meme.jpg" className="img0"/>
-
+    <>
+      
+      <div className='status'>{status}</div>
       {/**Divs to contain rows of squares*/}
       <div className='board-row'>
         <Square val={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -94,8 +96,42 @@ function App() {
         <Square val={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square val={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+
+    </>
+  );
+}
+
+/**
+ * Game: function that will hold our entire website and handle game play 
+ */
+function Game(){
+  //setting up turns and array that will hold our move history
+  const [xNext, setXNext] = useState(true);
+  const [hist, setHist] = useState([Array(9).fill(null)]);
+  const currSquares = hist[hist.length - 1]; //square array to be displayed
+
+  /**
+   * handlePlay: toggles xNext and updates hist -array of board states representing history of the board- w/ nextSquares
+   * Input: nextSquares- new state of the board
+   */
+  function handlePlay(nextSquares){
+    setHist([...hist, nextSquares]);
+    setXNext(!xNext);
+  }
+
+  /* Everything in the website must be wrapped within an enclosing tag... In this case, the div tag.
+   * This is b/c only a single element may be returned  */
+  return(
+    <div className="game">
+
+      <h1 className='h1'>Hi! Welcome to my version of the React Tic-Tac-Toe Tutorial. Click on the board to start the game!</h1>
+      <Board xNext={xNext} squares={currSquares} onPlay={handlePlay} />
+      <div className='game-info'>
+        <ol>{/**TODO */}</ol>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default Game;
